2f43d45829e0e3cf50d50ed4d3e5fcde79bc96d1
yutadayone <129708410+yutadayone@users.noreply.github.com>
2023-05-07 11:21:40 +0000

    応用課題４

--- rails6-bookers2-debug-ruby3-main/config/routes.rb
+++ rails6-bookers2-debug-ruby3-main/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   #get 'favorites/create'
   #get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -12,5 +14,10 @@ Rails.application.routes.draw do
   resources :book_comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  # ネストさせる
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
